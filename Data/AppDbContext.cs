8968fa6cd44a623fcfa86ef7ccf151ee630a2345
muhamad miftahul ulum <ulumm3952@gmail.com>
2025-11-01 09:07:39 +0700

    update3

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace AplikasiPemesananBus_UAS.Migration
+namespace AplikasiPemesananBus_UAS.Data
 {
     internal class AppDbContext
     {
